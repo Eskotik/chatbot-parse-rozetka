a61a3f07ea81554e5479f2eb1dff32f8c378f19b
Sasha Voitsekhivskyi <AlexVoitsekh@gmail.com>
2023-02-20 01:25:38 +0200

    update regex and added userid in data

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ const { JSDOM } = require("jsdom");
 
 bot.setMyCommands([{ command: "/start", description: "Привітання" }]);
 
-async function parse(chatId, url) {
+async function parse(chatId, url, userid) {
   try {
     const dom = await JSDOM.fromURL(url);
     const d = dom.window.document;
     let goods = d.querySelector("rz-product.ng-star-inserted");
     let goodsId = goods.querySelector(
       "p.product__code.detail-code"
-    ).textContent;
+    ).textContent.slice(5)
     let goodsName = goods.querySelector("h1.product__title").textContent;
-    let goodsPrice = goods
+    let goodsPrice = parseFloat(goods
       .querySelector("p.product-prices__big")
       .textContent.split(/\s+/)
-      .join("");
+      .join(""));
     let goodsPhoto = goods
       .querySelector("img.picture-container__picture")
       .getAttribute("src");
     let goodsStatus = goods.querySelector("p.status-label").textContent;
-
+  data = {
+    value1: userid,
+    value2: goodsId,
+    value3: goodsName,
+    value4: goodsPrice,
+    value5: goodsPhoto,
+    value6: goodsStatus
+}
     bot.sendMessage(
       chatId,
-      `id товару:${goodsId.slice(5)}\n
-      Назва:${goodsName}\n
-      Ціна: ${parseFloat(goodsPrice)}₴\n
-      Статус:${goodsStatus}`
+      `Id юзера:${userid}\nId товару:${goodsId}\nНазва:${goodsName}\nЦіна: ${goodsPrice}₴\nСтатус:${goodsStatus}`
     );
   } catch (e) {
     bot.sendMessage("Error");
@@ -48,7 +52,7 @@ const start = async () => {
   bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
-
+    const userid = msg.from.id;
     if (text === "/start") {
       await bot.sendMessage(
         chatId,
@@ -62,9 +66,11 @@ const start = async () => {
 
     try {
       const url = msg.text;
+      console.log(msg)
+      if (/^https?:\/\/.*?rozetka\.com\.ua\//.test(url)) {
+        await parse(chatId, url, userid);
+        bot.sendMessage(chatId, "✅Дані занесено в базу");
 
-      if (/^https?:\/\/rozetka\.com\.ua\//.test(url)) {
-        await parse(chatId, url);
       }
     } catch (e) {
       bot.sendMessage(chatId, "Error");
